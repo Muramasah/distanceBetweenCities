083d564833c2e25e9422e9e2b37e3ea3e43f3d31
Dielentheis <elizabeth.dielentheis@gmail.com>
2016-11-22 15:34:09 -0800

    updated

--- cities.spec.js
+++ cities.spec.js
@@ -2,45 +2,45 @@ var distanceFuncs = require('./distanceBetweenCities.js');
 
 describe('City name to coordinate conversion', function () {
 
-	it('correctly assigns latitude and longitude values', function(done) {
-		var coordsPromise = distanceFuncs.assignCityCoords([{name: 'Copenhagen', coordinates: null}, {name: 'Aarhus', coordinates: null}]);
-		coordsPromise.then(function(coords) {
-			expect(coords[0].coordinates.lat).toBe(55.6760968);
-			expect(coords[0].coordinates.lng).toBe(12.5683371);
-			expect(coords[1].coordinates.lat).toBe(56.162939);
-			expect(coords[1].coordinates.lng).toBe(10.203921);
-			done();
-		});
-	});
+    it('correctly assigns latitude and longitude values', function(done) {
+        var coordsPromise = distanceFuncs.assignCityCoords([{name: 'Copenhagen', coordinates: null}, {name: 'Aarhus', coordinates: null}]);
+        coordsPromise.then(function(coords) {
+            expect(coords[0].coordinates.lat).toBe(55.6760968);
+            expect(coords[0].coordinates.lng).toBe(12.5683371);
+            expect(coords[1].coordinates.lat).toBe(56.162939);
+            expect(coords[1].coordinates.lng).toBe(10.203921);
+            done();
+        });
+    });
 
 });
 
 describe('Distance calculator', function() {
 
-	var city1 = {name: 'Aarhus', coordinates: {lat: 56.162939, lng: 10.203921}};
-	var city2 = {name: 'London', coordinates: {lat: 51.5073509, lng: -0.1277583}};
-	var city3 = {name: 'Sydney', coordinates: {lat: -33.8688197, lng: 151.2092955}};
+    var city1 = {name: 'Aarhus', coordinates: {lat: 56.162939, lng: 10.203921}};
+    var city2 = {name: 'London', coordinates: {lat: 51.5073509, lng: -0.1277583}};
+    var city3 = {name: 'Sydney', coordinates: {lat: -33.8688197, lng: 151.2092955}};
 
-	it('correctly determines closest cities', function() {
-		var result = distanceFuncs.compareDistances([city1, city2, city3]);
-		expect(result.length).toBe(1);
-		expect(result[0][0].name).toBe('Aarhus');
-		expect(result[0][1].name).toBe('London');
-	});
+    it('correctly determines closest cities', function() {
+        var result = distanceFuncs.compareDistances([city1, city2, city3]);
+        expect(result.length).toBe(1);
+        expect(result[0][0].name).toBe('Aarhus');
+        expect(result[0][1].name).toBe('London');
+    });
 
-	it('can handle when more than two cities are the same distance apart', function() {
-		var result = distanceFuncs.compareDistances([city2, city2, city2]);
-		expect(result.length).toBe(3);
-	});
+    it('can handle when more than two cities are the same distance apart', function() {
+        var result = distanceFuncs.compareDistances([city2, city2, city2]);
+        expect(result.length).toBe(3);
+    });
 
 });
 
 describe('Print function', function() {
 
-	it('console.logs city names', function() {
-		console.log = jasmine.createSpy("log");
-		distanceFuncs.printCities([[{name: 'City One'}, {name: 'City Two'}]])
-		expect(console.log).toHaveBeenCalled();
-	});
+    it('console.logs city names', function() {
+        console.log = jasmine.createSpy('log');
+        distanceFuncs.printCities([[{name: 'City One'}, {name: 'City Two'}]])
+        expect(console.log).toHaveBeenCalled();
+    });
 
 });
--- distanceBetweenCities.js
+++ distanceBetweenCities.js
@@ -6,102 +6,104 @@ readCities();
  * @return {Array} cities
  */
 function readCities() {
-	var fs = require('fs');
-	fs.readFile('./cities.txt', function(err, data) {
-		if (err) {
-			throw new Error(err);
-		}
-		else {
-			var cities = data.toString().split('\n');
-			cities.forEach(function(city, i, cities) {
-				cities[i] = {name: city, coordinates: null};
-			});
-			assignCityCoords(cities);
-			return cities;
-		}
-	});
+    var fs = require('fs');
+    fs.readFile('./cities.txt', function(err, data) {
+        if (err) {
+            throw new Error(err);
+        }
+        else {
+            var cities = data.toString().split('\n');
+            cities.forEach(function(city, i, cities) {
+                cities[i] = {name: city, coordinates: null};
+            });
+            assignCityCoords(cities);
+            return cities;
+        }
+    });
 }
 
 /**
  * Finds each city's coordinates based on city name, then calls compareDistances
  * @param {Array} cities (just names)
- * @return {Array} cities (names and coordinates)
+ * @return {Promise} Array of cities (names and coordinates) - for testing
  */
 function assignCityCoords(cities) {
-	var Promise = require('bluebird');
-	var config = require('./config.js');
-	var googleMapsClient = require('@google/maps').createClient({
-	  key: config.GOOGLE,
-	  Promise: Promise
-	});
-	var coordPromises = [];
+    var Promise = require('bluebird');
+    var config = require('./config.js');
+    var googleMapsClient = require('@google/maps').createClient({
+      key: config.GOOGLE,
+      Promise: Promise
+    });
+    var coordPromises = [];
 
-	if (cities.length < 2) {
-		throw new Error('Please specify 2 or more cities!');
-	}
+    if (cities.length < 2) {
+        throw new Error('Please specify 2 or more cities!');
+    }
 
-	// promisifies each API call and adds it to array
-	for (var i = 0; i < cities.length; i++) {
-		coordPromises.push(googleMapsClient.geocode({
-				address: cities[i].name
-		}).asPromise());
-	}
+    // promisifies each API call and adds it to array
+    for (var i = 0; i < cities.length; i++) {
+        coordPromises.push(googleMapsClient.geocode({
+                address: cities[i].name
+        }).asPromise());
+    }
 
-	// makes API calls/resolves all promises, then calls compareDistances function
-	return Promise.all(coordPromises)
-	.then(function(response) {
-		response.forEach(function(cityData, i) {
-			cities[i].coordinates = cityData.json.results[0].geometry.location;
-		});
-		compareDistances(cities);
-		return cities;
-	})
-	.catch(function(err) { throw new Error(err) });
+    // makes API calls/resolves all promises, then calls compareDistances function
+    return Promise.all(coordPromises)
+    .then(function(response) {
+        response.forEach(function(cityData, i) {
+            if (!cityData.json.results[0]) {
+                throw new Error(cities[i].name + " is not a valid city. Please remove this city and try again.");
+            }
+            cities[i].coordinates = cityData.json.results[0].geometry.location;
+        });
+        compareDistances(cities);
+        return cities;
+    })
+    .catch(function(err) { throw new Error(err) });
 }
 
 /**
  * Finds the two cities with the minimum distance between them, then calls print function
  * @param {Array} city objects
- * @return {2D Array} city object tuples
+ * @return {2D Array} city object tuples - for testing
  */
 function compareDistances(cities) {
-	var geolib = require('geolib');
-	var halfOfEarthsCircumference = 20000000;
-	var minDistance = halfOfEarthsCircumference;
-	var minCities = [];
+    var geolib = require('geolib');
+    var halfOfEarthsCircumference = 20000000; // the furthest apart any cities could possibly be (in meters)
+    var minDistance = halfOfEarthsCircumference;
+    var minCities = [];
 
-	// compares distance to the minimum distance so far, determines minCities
-	for (var i = 0; i < cities.length - 1; i++) {
-		for (var j = i + 1; j < cities.length; j++) {
-			var distance = geolib.getDistance({latitude: cities[i].coordinates.lat, longitude: cities[i].coordinates.lng}, {latitude: cities[j].coordinates.lat, longitude: cities[j].coordinates.lng});
-			if (distance < minDistance) {
-				minDistance = distance;
-				minCities = [[cities[i], cities[j]]];
-			}
-			else if (distance === minDistance) {
-				minCities.push([cities[i], cities[j]]);
-			}
-		}
-	}
-	printCities(minCities);
-	return minCities;
+    // compares distance to the minimum distance so far, determines minCities
+    for (var i = 0; i < cities.length - 1; i++) {
+        for (var j = i + 1; j < cities.length; j++) {
+            var distance = geolib.getDistance({latitude: cities[i].coordinates.lat, longitude: cities[i].coordinates.lng}, {latitude: cities[j].coordinates.lat, longitude: cities[j].coordinates.lng});
+            if (distance < minDistance) {
+                minDistance = distance;
+                minCities = [[cities[i], cities[j]]];
+            }
+            else if (distance === minDistance) {
+                minCities.push([cities[i], cities[j]]);
+            }
+        }
+    }
+    printCities(minCities);
+    return minCities;
 }
 
 /**
- * Prints city names
+ * Prints city names - will print multiple lines if there is more than one pair that has the same distance apart
  * @param {2D Array} city object tuples
  */
 function printCities(cityTuples) {
-	// in case there is more than one pair that has the same distance apart
-	cityTuples.forEach(function(tuple, i) {
-		console.log(cityTuples[i][0].name, 'and', cityTuples[i][1].name);
-	});
+    cityTuples.forEach(function(tuple, i) {
+        console.log(cityTuples[i][0].name, 'and', cityTuples[i][1].name);
+    });
 }
 
 // for testing purposes
 module.exports = {
-	readCities: readCities,
-	assignCityCoords: assignCityCoords,
-	compareDistances: compareDistances,
-	printCities: printCities
+    readCities: readCities,
+    assignCityCoords: assignCityCoords,
+    compareDistances: compareDistances,
+    printCities: printCities
 };
